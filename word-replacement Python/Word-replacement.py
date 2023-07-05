15e36c5765ff7779795dc2686ed2f80a3526b9ae
Adam Power Gonzalez <72943449+mrpowergonz@users.noreply.github.com>
2023-07-05 14:58:08 +0200

    Add files via upload

---
+++ word-replacement Python/Word-replacement.py
@@ -0,0 +1,9 @@
+def replace_word():
+
+    str= "Hi guys, I am Adam, and hi hi hi hi "
+    word_to_replace = input("Enter the word to replace: ")
+    word_repleacement = input("Enter the word to repleacement: ") 
+    print(str.replace(word_to_replace, word_repleacement))
+
+replace_word()
+
